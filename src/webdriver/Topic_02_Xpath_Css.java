157948cbc7ea51042ec4a3ad608ce7b6a47d8387
Ha <Ha@Ha-PC>
2019-12-31 16:50:29 +0700

    Topic_03

--- src/webdriver/Topic_02_Xpath_Css.java
+++ src/webdriver/Topic_02_Xpath_Css.java
@@ -3,6 +3,8 @@ package webdriver;
 import org.testng.annotations.Test;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
+
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -46,8 +48,8 @@ public class Topic_02_Xpath_Css {
 	   driver.findElement(By.xpath("//div[@class='footer']//a[text()='My Account']")).click();
 	   driver.findElement(By.cssSelector("#email")).sendKeys("132132@13131.13131");
 	   driver.findElement(By.id("send2")).click();
-	   String usernameInvalidMessage = driver.findElement(By.id("advice-required-entry-email")).getText();
-	   Assert.assertEquals(usernameInvalidMessage, "Please enter a valid email address. For example johndoe@domain.com.");
+	   String usernameInvalidMessage = driver.findElement(By.id("advice-validate-email-email")).getText();
+	   Assert.assertEquals(usernameInvalidMessage,"Please enter a valid email address. For example johndoe@domain.com.");
  	  
 	   
 	  
@@ -58,15 +60,28 @@ public class Topic_02_Xpath_Css {
 	   driver .get("http://live.demoguru99.com/index.php");
 	   driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	   driver.findElement(By.xpath("//div[@class='footer']//a[text()='My Account']")).click();
-	   driver.findElement(By.cssSelector("#pass")).sendKeys("132132");
+	   driver.findElement(By.cssSelector("#pass")).sendKeys("13132");
 	   driver.findElement(By.id("send2")).click();
-	   String passwordIncorrectMessage = driver.findElement(By.id("#advice-required-entry-pass")).getText();
-	   Assert.assertEquals(passwordIncorrectMessage, "Please enter a valid email address. For example johndoe@domain.com.");
+	   String passwordIncorrectMessage = driver.findElement(By.id("advice-validate-password-pass")).getText();
+	   Assert.assertEquals(passwordIncorrectMessage, "Please enter 6 or more characters without leading or trailing spaces.");
 	   
+   }
+
+   
+   @Test
+   public void TC_04_CreatAnAccount() {
+	   driver .get("http://live.demoguru99.com/index.php");
+	   driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	   driver.findElement(By.xpath("//div[@class='footer']//a[text()='My Account']")).click();
+	   
+
+	   driver.findElement(By.cssSelector("#email")).sendKeys("automation"+ randomEmail() + "@gmail.com");
+	   // automation1243@gmail.com
 	 
  	  
  	  
    }
+
 
    
   @AfterClass
@@ -74,5 +89,10 @@ public class Topic_02_Xpath_Css {
 	  driver.quit();
 	  
   }
-
+public int randomEmail() {
+	Random random = new Random();
+	int number = random.nextInt(999999);
+	 return number;
+	
+}
 }
---
+++ src/webdriver/Topic_03_WebElement_Browser.java
@@ -0,0 +1,27 @@
+package webdriver;
+
+import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+
+public class Topic_03_WebElement_Browser {
+	WebDriver driver;
+	
+  @Test
+  public void TC_01_WebElement_Browser() {
+	  WebDriver driver;
+	 
+	  
+  }
+  
+  @Test
+  public void TC_01() {
+	 	  
+	  
+	 
+  }
+}
